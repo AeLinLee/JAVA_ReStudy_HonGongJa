cf1720f85c84da2af2f328d9b7e54204381f31c5
AeLinLee <dldofls666@gmail.com>
2025-02-17 20:45:26 +0900

    실행문이란?

---
+++ src/chap01/Sec03_2_실행문.java
@@ -0,0 +1,18 @@
+package chap01;
+
+public class Sec03_2_실행문 {
+
+	public static void main(String[] args) { 
+		
+		// 실행문: 변수 선언, 값 저장, 메소드 호출
+		
+		int x = 2;
+		int y = 3;
+		int result = x+y;
+
+	     System.out.println(result);
+	    
+
+	}
+
+}
